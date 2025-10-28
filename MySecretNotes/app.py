7d1306949004e821b09df6878d01d3efd13dd350
Adrian <adju@itu.dk>
2025-10-28 20:26:33 +0100

    Revert "Prevented SQL injection in app.py"
    This reverts commit ff6b2a25d86dc6d9d28edb00224610f758bee2fc.

--- MySecretNotes/app.py
+++ MySecretNotes/app.py
@@ -73,22 +73,22 @@ def notes():
             note = request.form['noteinput']
             db = connect_db()
             c = db.cursor()
-            statement = """INSERT INTO notes(id,assocUser,dateWritten,note,publicID) VALUES(null,?,'?','?',?);"""
+            statement = """INSERT INTO notes(id,assocUser,dateWritten,note,publicID) VALUES(null,%s,'%s','%s',%s);""" %(session['userid'],time.strftime('%Y-%m-%d %H:%M:%S'),note,random.randrange(1000000000, 9999999999))
             print(statement)
-            c.execute(statement, (session['userid'],time.strftime('%Y-%m-%d %H:%M:%S'),note,random.randrange(1000000000, 9999999999)))
+            c.execute(statement)
             db.commit()
             db.close()
         elif request.form['submit_button'] == 'import note':
             noteid = request.form['noteid']
             db = connect_db()
             c = db.cursor()
-            statement = """SELECT * from NOTES where publicID = ?"""
-            c.execute(statement, (noteid,))
+            statement = """SELECT * from NOTES where publicID = %s""" %noteid
+            c.execute(statement)
             result = c.fetchall()
             if(len(result)>0):
                 row = result[0]
-                statement = """INSERT INTO notes(id,assocUser,dateWritten,note,publicID) VALUES(null,?, ?, ? ,?);"""
-                c.execute(statement, (session['userid'],row[2],row[3],row[4]))
+                statement = """INSERT INTO notes(id,assocUser,dateWritten,note,publicID) VALUES(null,%s,'%s','%s',%s);""" %(session['userid'],row[2],row[3],row[4])
+                c.execute(statement)
             else:
                 importerror="No such note with that ID!"
             db.commit()
@@ -96,9 +96,9 @@ def notes():
     
     db = connect_db()
     c = db.cursor()
-    statement = "SELECT * FROM notes WHERE assocUser = ?;"  #Should not be secured
+    statement = "SELECT * FROM notes WHERE assocUser = %s;" %session['userid']
     print(statement)
-    c.execute(statement, (session['userid'],))
+    c.execute(statement)
     notes = c.fetchall()
     print(notes)
     
@@ -113,8 +113,8 @@ def login():
         password = request.form['password']
         db = connect_db()
         c = db.cursor()
-        statement = "SELECT * FROM users WHERE username = ? AND password = ?;"
-        c.execute(statement, (username, password))
+        statement = "SELECT * FROM users WHERE username = '%s' AND password = '%s';" %(username, password)
+        c.execute(statement)
         result = c.fetchall()
 
         if len(result) > 0:
@@ -147,9 +147,9 @@ def register():
             passworderror = "That account already exists!"
 
         if(not errored):
-            statement = """INSERT INTO users(id,username,password) VALUES(null, ?, ?);"""
+            statement = """INSERT INTO users(id,username,password) VALUES(null,'%s','%s');""" %(username,password)
             print(statement)
-            c.execute(statement, (username, password))
+            c.execute(statement)
             db.commit()
             db.close()
             return f"""<html>
